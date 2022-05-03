d905598cb492dddd1b431bde62c0e7661d04beab
Giordano Andreola <gio.andreola@stud.uniroma3.it>
2022-05-03 23:13:28 +0200

    Commit prepotente

---
+++ siw-servlet-es0/src/main/webapp/persona.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>persona</title>
+  </head>
+  <body>
+    <h1>Hai inserito questi dati:</h1>
+    <div>Nome: ${nome}</div>
+    <div>Cognome: ${cognome}</div>
+  </body>
+</html>
---
+++ siw-servlet-es1/src/main/java/it/uniroma3/siw/controller/validator/PersonaValidator.java
@@ -0,0 +1,27 @@
+package it.uniroma3.siw.controller.validator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PersonaValidator {
+
+	public boolean valid(HttpServletRequest request) {
+		boolean isValid = true;
+		String nome = request.getParameter("nome");
+		String cognome = request.getParameter("cognome");
+		// gestione della RISPOSTA
+		Map<String, String> messaggiErrore = new HashMap<>();
+		if (nome.isBlank()) {
+			messaggiErrore.put("nome", "inserire un nome");
+			isValid = false;
+		}
+		if (cognome.isBlank()) {
+			messaggiErrore.put("cognome", "inserire un cognome");
+			isValid = false;
+		}
+		request.setAttribute("messaggiErrore", messaggiErrore);
+		return isValid;
+	}
+}
